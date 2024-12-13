dfb2213389d331df3f0dbb0c5c35658054a254bb
ggraham4 <142766544+ggraham4@users.noreply.github.com>
2024-12-13 12:12:42 -0600

    Changed excel file to .csv

--- Linear Regressions.R
+++ Linear Regressions.R
@@ -14,8 +14,8 @@ library(lmerTest)
   library(readxl)
 
   {
-  data <- read_excel("Assays Data/Assays Data.xlsx", na = "NA")
-data <- subset(data,Sex != "G" & (Rater == "GG"| Rater == "EP" | Rater == "IW")& #Subset to all videos that have been rated
+ data <- read.csv("Assays Data.csv", na = "NA")
+    data <- subset(data,Sex != "G" & (Rater == "GG"| Rater == "EP" | Rater == "IW")& #Subset to all videos that have been rated
                  Tank != "T12"& 
                  Tank != "B11" &
                  Tank != "B21"& 
